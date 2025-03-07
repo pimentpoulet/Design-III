c1604cffab2b2ab2bcb7ece878b282851e7abb42
Clément Poulin <117486857+pimentpoulet@users.noreply.github.com>
2025-03-07 02:27:03 -0500

    Class encapsulation
    Added methods :
    - click_clear
    - click_start
    - update_loop
    - save_data
    - display_data

--- UI/UI.py
+++ UI/UI.py
@@ -21,60 +21,102 @@ class TextRedirector:
         pass
 
 
+def load_data():
+    file_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
+    if not file_path:
+        return None
+
+    try:
+        wb = xl.Book(file_path)
+        sheet = wb.sheets[0]
+        raw_data = sheet["A40:A540"].options(np.array, expand="table").value
+        wb.close()
+        split_arr = np.char.split(raw_data, ',')
+        return np.array([x[0] for x in split_arr], dtype=float), np.array([x[1] for x in split_arr], dtype=float), file_path
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        return None
+
+
+def print_grid_info(root):
+    for child in root.winfo_children():
+        info = child.grid_info()
+        if info:
+            print(f"Widget: {child}, Row: {info['row']}, Column: {info['column']}")
+
+
 class PowerMeterApp:
     def __init__(self, root):
+
+        self.root = root
+        self.root.title("Powermeter UI")
+        # self.root.state("zoomed")
+        self.root.resizable(True, True)
+
         self.power = None
         self.fig = None
         self.ax1 = None
         self.ax2 = None
         self.canvas = None
         self.log_text = None
-        self.root = root
-        self.root.title("Power Meter UI")
-        self.root.geometry("1800x800")
-        self.root.resizable(True, True)
 
-        self.app = xl.App(visible=False)
+        # global variables
         self.wavelengths_1 = None
         self.power_values_1 = None
         self.wavelengths_2 = None
         self.power_values_2 = None
+
+        self.app = xl.App(visible=False)
 
         self.setup_ui()
 
     def setup_ui(self):
         mainframe = ttk.Frame(self.root, padding="20 20 20 20")
         mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+
+        # 4 even columns
         self.root.columnconfigure(0, weight=1)
+        self.root.columnconfigure(1, weight=1)
+        self.root.columnconfigure(2, weight=1)
+        self.root.columnconfigure(3, weight=1)
+
+        # 4 even rows
         self.root.rowconfigure(0, weight=1)
+        self.root.rowconfigure(1, weight=1)
+        self.root.rowconfigure(2, weight=1)
+        self.root.rowconfigure(3, weight=1)
 
         self.power = tk.StringVar()
         power_entry = ttk.Entry(mainframe, width=7, textvariable=self.power)
-        power_entry.grid(column=1, row=1, sticky=(tk.W, tk.E))
-        ttk.Label(mainframe, text="Power [mW]").grid(column=2, row=1, sticky=tk.W)
+        power_entry.grid(column=0, row=0, sticky=(tk.W, tk.E))
+        ttk.Label(mainframe, text="Power [mW]").grid(column=1, row=0, sticky=tk.N)
+        ttk.Label(mainframe, text="Test Position").grid(column=0, row=3)
 
-        ttk.Button(self.root, text="Load Data 1", command=lambda: self.display_data_1(self.load_data())).grid(column=0,
-                                                                                                              row=1,
-                                                                                                              columnspan=2,
-                                                                                                              pady=10)
-        ttk.Button(self.root, text="Load Data 2", command=lambda: self.display_data_2(self.load_data())).grid(column=1,
-                                                                                                              row=1,
-                                                                                                              columnspan=2,
-                                                                                                              pady=10)
-        ttk.Button(self.root, text="Save Data 1", command=self.save_data_1).grid(column=0, row=2, columnspan=2, pady=10)
+        # data loading buttons
+        ttk.Button(self.root, text="Load Data 1", command=lambda: self.display_data_1(load_data())).grid(column=0,
+                                                                                                         row=2,
+                                                                                                         sticky=tk.W,
+                                                                                                         pady=10)
+        ttk.Button(self.root, text="Load Data 2", command=lambda: self.display_data_2(load_data())).grid(column=1,
+                                                                                                         row=2,
+                                                                                                         sticky=tk.W,
+                                                                                                         pady=10)
+        # saving button
+        ttk.Button(self.root, text="Save Data 1", command=self.save_data_1).grid(column=0, row=2, pady=10)
 
-        self.fig = Figure(figsize=(14, 6), dpi=100)
+        self.fig = Figure(figsize=(7, 2), dpi=100)
         self.ax1 = self.fig.add_subplot(121)
-        self.ax2 = self.fig.add_subplot(122)
+        # self.ax2 = self.fig.add_subplot(122)
 
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
-        self.canvas.get_tk_widget().grid(column=0, row=3, columnspan=3, sticky=(tk.N, tk.W, tk.E, tk.S))
+        self.canvas.get_tk_widget().grid(column=0, row=1, sticky=tk.W)
 
-        log_frame = ttk.LabelFrame(self.root, text="Log Output")
-        log_frame.grid(column=0, row=4, columnspan=3, sticky=(tk.W, tk.E, tk.S), pady=10, padx=10)
+        # terminal setup
+        log_frame = ttk.LabelFrame(self.root, text="Terminal")
+        log_frame.grid(column=0, row=4, sticky=(tk.W, tk.E, tk.S), pady=20, padx=20)    # sticky OK
 
         self.log_text = tk.Text(log_frame, height=10, width=120, wrap="word")
-        self.log_text.grid(column=0, row=0, sticky=(tk.W, tk.E, tk.S, tk.N))
+        self.log_text.grid(column=0, row=0)
         self.log_text.insert(tk.END, "Log initialized...\n")
 
         scrollbar = ttk.Scrollbar(log_frame, orient="vertical", command=self.log_text.yview)
@@ -83,21 +125,8 @@ class PowerMeterApp:
 
         sys.stdout = TextRedirector(self.log_text)
 
-    def load_data(self):
-        file_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
-        if not file_path:
-            return None
-
-        try:
-            wb = xl.Book(file_path)
-            sheet = wb.sheets[0]
-            raw_data = sheet["A40:A540"].options(np.array, expand="table").value
-            wb.close()
-            split_arr = np.char.split(raw_data, ',')
-            return np.array([x[0] for x in split_arr], dtype=float), np.array([x[1] for x in split_arr], dtype=float), file_path
-        except Exception as e:
-            print(f"Error loading data: {e}")
-            return None
+        # debugging
+        print_grid_info(self.root)
 
     def display_data_1(self, data_tuple):
         if data_tuple is None:
@@ -122,18 +151,22 @@ class PowerMeterApp:
             return
         self.wavelengths_2, self.power_values_2, file_path = data_tuple
 
-        self.ax2.clear()
-        self.ax2.plot(self.wavelengths_2, self.power_values_2, label="wpcurve", color="blue", linewidth=1)
-        self.ax2.set_xlabel("Wavelength [nm]")
-        self.ax2.set_ylabel("Power [dB]")
-        self.ax2.set_title("OSA data 2")
-        self.ax2.legend()
-        self.ax2.grid(True)
+        try:
+            self.ax2.clear()
+            self.ax2.plot(self.wavelengths_2, self.power_values_2, label="wpcurve", color="blue", linewidth=1)
+            self.ax2.set_xlabel("Wavelength [nm]")
+            self.ax2.set_ylabel("Power [dB]")
+            self.ax2.set_title("OSA data 2")
+            self.ax2.legend()
+            self.ax2.grid(True)
 
-        self.canvas.draw()
+            self.canvas.draw()
 
-        # log to terminal
-        print(f"loaded {str(file_path)}")
+            # log to terminal
+            print(f"loaded {str(file_path)}")
+
+        except Exception as e:
+            print(f"Error displaying data: {e}")
 
     def save_data_1(self):
         if self.wavelengths_1 is None or self.power_values_1 is None:
