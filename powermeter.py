310c4061e5be0355a4e75ec1f61fc4ba81c20033
Alexandre Beliveau <alexandre.beliveau.2@ulaval.ca>
2025-03-10 13:51:17 -0400

    Main de la calibration et fonction position powermeter

--- powermeter.py
+++ powermeter.py
@@ -2,6 +2,7 @@ from mlx90640_evb9064x import *
 from mlx90640 import *
 from calibration_class import *
 import numpy as np
+import cv2
 
 
 class PowerMeter:
@@ -51,6 +52,61 @@ class PowerMeter:
         return corrected_temp
 
 
+    def calc_centroid(self) -> tuple:
+        temp = self.get_moy_temp()
+        moy_temp = np.mean(temp)
+        delta_temp = temp - moy_temp
+        total_temp = np.sum(delta_temp)
+        
+        x_coords, y_coords = np.meshgrid(np.arange(self.cols), np.arange(self.rows))
+        x_centroid = np.sum(x_coords * delta_temp) / total_temp
+        y_centroid = np.sum(y_coords * delta_temp) / total_temp
+        
+        return (x_centroid, y_centroid)
+    
+
+    def find_circle(self):
+        temp = self.get_moy_temp()
+        circles = cv2.HoughCircles(temp, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=2, maxRadius=16)
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            return circles[0,0]
+        else:
+            return self.calc_centroid()
+        
+    def temp_gradient(self):
+        temp = self.get_moy_temp()
+        dx, dy = np.gradient(temp)
+        return dx, dy
+
+
+    def find_max_temp_index(self):
+        temp = self.get_moy_temp()
+        return np.unravel_index(np.argmax(temp, axis=None), temp.shape)
+    
+
+    def extrapolate_temp(self, temp) -> np.ndarray:
+        grad = self.temp_gradient()
+        pass
+
+
+    def checkers_grid(self, temp, sq_size, odd_even):
+        if odd_even not in (0, 1):
+            raise ValueError('odd_even must be 0 or 1')
+        grid = np.zeroslike(temp)
+        ind = np.indices(temp.shape)
+        grid[(ind[0] // sq_size + ind[1] // sq_size) % 2 == odd_even] = temp
+        return grid
+
+
+    def get_wavelength(self) -> float:
+        temp = self.get_moy_temp()
+        grid1 = self.checkers_grid(temp, 2, 0)
+        grid2 = self.checkers_grid(temp, 2, 1)
+        pass
+
+
     def get_power(self) -> float:
         temp = self.get_moy_temp()
+        diff = np.max(temp) - np.min(temp)
         pass
