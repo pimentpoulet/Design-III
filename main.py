a2b44f8d2ee384cc78ad62e91757cf76ad507c92
Alexandre Beliveau <alexandrebeliveau@icloud.com>
2025-03-11 15:30:02 -0400

    Finalisation des tests pour la calibration

--- main.py
+++ main.py
@@ -1,45 +1,62 @@
 from calibration_class import PowerMeter_calibration
+from powermeter import PowerMeter
 import numpy as np
 import os
 from time import sleep
-from scipy.optimize import curve_fit
+from affichage import show_image_opencv
+import cv2
+import sys
 
 
 def main_cal():
     temp_moys = []
     temps = []
     try:
-        for temp in range(20, 180, 5):
+        # Initialisation de la caméra
+        pm = PowerMeter_calibration(20, 60, 6)
+        for temp in range(20, 25, 5):
             print(f"Calibration pour {temp}°C")
-            print("Appuyez sur une touche lorsque le corps noir est à température")
-            input()
-            # Initialisation de la caméra
-            pm = PowerMeter_calibration(temp, 60, 6)
+            input("Appuyez sur une touche lorsque le corps noir est à température\n")
+            
+            pm.ref_temp = temp
             # Calibration
-            for frame in range(pm.number_of_frames+pm.remove_frames):
+            for frame in range(pm.number_of_frames + pm.remove_frames):
+                #print(f"Frame {frame}")
                 if frame == pm.remove_frames:
                     print("Début de la calibration")
                 if frame >= pm.remove_frames:
-                    for i in range(3):
-                        try: 
-                            pm.update_temperature(pm.get_temp())
-                            break
-                        except ValueError as e:
-                            print(f"Erreur de communication avec le capteur: {e}")
-                            sleep(1)
+                    try:
+                        captured_temp = pm.get_temp()
+                        pm.update_temperature(captured_temp)
+                        show_image_opencv(captured_temp)
+                        #break
+                    except ValueError as e:
+                        print(f"Erreur de communication avec le capteur à la frame {frame}: {e}")
+                        sleep(1)
+                        continue
+                    except Exception as e:
+                        print(f"Unexpected error at frame {frame}: {e}")
+                        sleep(1)
+                        continue
                 else:
-                    for i in range(3):
-                        try: 
-                            pm.get_temp()
-                            break
-                        except ValueError as e:
-                            print(f"Erreur de communication avec le capteur: {e}")
-                            sleep(1)
+                    try:
+                        show_image_opencv(pm.get_temp())
+                        #break
+                    except ValueError as e:
+                        print(f"Erreur de communication avec le capteur à la frame {frame}: {e}")
+                        sleep(1)
+                        continue
+                    except Exception as e:
+                        print(f"Unexpected error at frame {frame}: {e}")
+                        sleep(1)
+                        continue
             temp_moys.append(pm.get_moy_temp())
-            temps.append(temp) 
+            temps.append(temp)
             print("Calibration terminée")
     except ValueError as e:
         print(f"Erreur lors de la calibration: {e}")
+    except Exception as e:
+        print(f"Unexpected error during calibration: {e}")
 
     # Sauvegarde des données
     if not os.path.exists("calibration_data"):
@@ -49,7 +66,7 @@ def main_cal():
     np.save("temps.npy", np.array(temps))
     os.chdir("..")
     print("Données sauvegardées")
-    
+
 
 def main_fit_cal():
     # Chargement des données
@@ -59,7 +76,7 @@ def main_fit_cal():
     os.chdir("calibration_data")
     temp_moys = np.load("temp_diffs.npy")
     temps = np.load("temps.npy")
-    if not temp_moys or not temps:
+    if not np.all(temp_moys) or not temps:
         print("Pas de données de calibration")
         return
     height, width = temp_moys.shape[1], temp_moys.shape[2]
@@ -73,6 +90,26 @@ def main_fit_cal():
             coeffs_array[:, i, j] = np.polyfit(temp_moys[:, i, j], temps, deg=4)
     np.save(f"coeffs_range{temps[0]}-{temps[-1]}_jump{(temps[-1]-temps[0])/temps.size}.npy", coeffs_array)
     os.chdir("..")
+    return coeffs_array
+
+
+def test_cal(calibration_data: np.ndarray):
+    pm = PowerMeter()
+    pm.coeffs_array = calibration_data
+    while True:
+        for i in range(32):
+            pm.update_temperature(pm.get_temp())
+            sleep(1/30)
+        corrected_temp = pm.calibrate_temp(pm.get_moy_temp())
+        show_image_opencv(corrected_temp)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            print("Fermeture détectée. Arrêt du programme...")
+            cv2.destroyAllWindows()
+            sys.exit(0)
 
 if __name__ == "__main__":
-    main_cal()
+    #main_cal()
+    #ca = main_fit_cal()
+    
+    ca = np.load("calibration_data/coeffs_range20-20_jump0.0.npy")
+    test_cal(ca)
